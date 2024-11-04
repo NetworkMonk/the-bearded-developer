ddcd3a389b29451bf53b9575aeefc6eb0ef622e0
NetworkMonk <jamesplant@gmail.com>
2024-11-04 13:08:28 +0000

    Additional error output in API route

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,21 +1,28 @@
 import sgMail from "@sendgrid/mail";
 
 export async function POST(req, res) {
-  const formData = await req.json();
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: "info@thebeardeddeveloper.co.uk", // Change to your recipient
-    from: "info@thebeardeddeveloper.co.uk", // Change to your verified sender
-    subject: "Website Submission",
-    text: JSON.stringify(formData, null, 2),
-    html: JSON.stringify(formData, null, 2),
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      return Response.json({ message: "Email sent" });
-    })
-    .catch((error) => {
-      return Response.json({ message: "Email not sent", error: error.message });
-    });
+  try {
+    const formData = await req.json();
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: "info@thebeardeddeveloper.co.uk", // Change to your recipient
+      from: "info@thebeardeddeveloper.co.uk", // Change to your verified sender
+      subject: "Website Submission",
+      text: JSON.stringify(formData, null, 2),
+      html: JSON.stringify(formData, null, 2),
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        return Response.json({ message: "Email sent" });
+      })
+      .catch((error) => {
+        return Response.json({
+          message: "Email not sent",
+          error: error.message,
+        });
+      });
+  } catch (e) {
+    return Response.json({ message: "Email not sent", error: e.message });
+  }
 }
