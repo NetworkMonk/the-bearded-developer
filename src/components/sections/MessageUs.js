e66fa0b1e60addf85de5c87ab2c052f2270a4d46
NetworkMonk <jamesplant@gmail.com>
2024-11-26 13:11:30 +0000

    Improved responsiveness of MessageUs component

--- src/components/sections/MessageUs.js
+++ src/components/sections/MessageUs.js
@@ -11,14 +11,14 @@ export default function MessageUs() {
         <div className="my-5">
           <InView>
             <div
-              className="p-5 py-8 mt-5 md:mt-10 md:p-10 bg-gradient-to-br from-brand-blue to-brand-light-blue text-white rounded-xl shadow-xl in-up flex items-center"
+              className="p-5 py-8 mt-5 md:mt-10 md:p-10 bg-gradient-to-br from-brand-blue to-brand-light-blue text-white rounded-xl shadow-xl in-up flex flex-col md:flex-row items-center gap-5"
               style={{ animationDelay: "0.25s" }}
             >
-              <p className="text-xl font-semibold">
+              <p className="text-xl font-semibold text-center md:text-left">
                 Don&apos;t forget to message us for a free consultation!
               </p>
               <button
-                className="w-48 outline outline-2 outline-white bg-transparent text-white hover:bg-white hover:text-brand-blue px-6 py-2 rounded-full transition-colors duration-500 uppercase font-semibold ml-auto"
+                className="w-48 outline outline-2 outline-white bg-transparent text-white hover:bg-white hover:text-brand-blue px-6 py-2 rounded-full transition-colors duration-500 uppercase font-semibold mx-auto md:ml-auto md:mr-0"
                 onClick={(e) => {
                   e.preventDefault();
                   document
