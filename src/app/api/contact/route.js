362d436257f10e83ddfaaa87e7caf197100986c6
NetworkMonk <jamesplant@gmail.com>
2025-07-18 12:54:25 +0100

    Refactor email sending in contact route: switch from SendGrid to SMTP2GO for email delivery

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,22 +1,36 @@
-import sgMail from "@sendgrid/mail";
-
 export async function POST(req, res) {
   try {
     const formData = await req.json();
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: "info@thebeardeddeveloper.co.uk", // Change to your recipient
-      from: "info@thebeardeddeveloper.co.uk", // Change to your verified sender
-      subject: "Website Submission",
-      text: JSON.stringify(formData, null, 2),
-      html: JSON.stringify(formData, null, 2),
-    };
-    try {
-      await sgMail.send(msg);
-      return Response.json({ message: "Email sent" });
-    } catch (e) {
-      return Response.json({ message: "Email not sent", error: e.message });
+    if (!formData || !formData.name || !formData.email || !formData.message) {
+      return Response.json({ message: "Invalid form data" }, { status: 400 });
     }
+
+    // Send email using SMTP2GO
+    const smtp2goUrl = `https://api.smtp2go.com/v3/email/send`;
+    const smtp2goHeaders = {
+      "Content-Type": "application/json",
+      "X-Smtp2go-Api-Key": `${process.env.SMTP2GO_API_KEY}`,
+      Accept: "application/json",
+    };
+    const smtp2goData = {
+      sender: "info@thebeardeddeveloper.co.uk",
+      to: ["info@thebeardeddeveloper.co.uk"],
+      subject: "Website Submission",
+      text_body: JSON.stringify(formData, null, 2),
+    };
+    const result = await fetch(smtp2goUrl, {
+      method: "POST",
+      headers: smtp2goHeaders,
+      body: smtp2goData ? JSON.stringify(smtp2goData) : null,
+    });
+    if (!result.ok) {
+      const errorData = await result.json();
+      return Response.json(
+        { message: "Email not sent", error: errorData },
+        { status: 500 }
+      );
+    }
+    return Response.json({ message: "Email sent successfully" });
   } catch (e) {
     return Response.json({ message: "Email not sent", error: e.message });
   }
