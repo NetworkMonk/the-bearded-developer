2463f48a8e991b336523640dc2ffabf3158a7a0e
James Plant <jamesplant@gmail.com>
2025-08-21 17:37:22 +0100

    Add custom solution section to ProjectsPage with contact call-to-action

--- src/app/resources/components/page.js
+++ src/app/resources/components/page.js
@@ -53,6 +53,27 @@ export default function ProjectsPage() {
               </div>
             </div>
           </section>
+
+          <section className="md:p-10 relative">
+            <h2
+              className={`text-5xl md:text-6xl font-bold ${lexend.className}`}
+            >
+              Need Something Custom?
+            </h2>
+            <p
+              className={`text-lg md:text-xl font-semibold mt-5 md:mt-10 max-w-xl`}
+            >
+              Let&apos;s work together to create a custom solution that meets
+              your needs. Contact us today.
+            </p>
+            <p className="mt-5">
+              <a href="/contact">
+                <button className="animated-button animated-button-fill animated-button-small min-w-36 text-brand-black hover:text-brand-platinum uppercase font-semibold transition-all duration-500 text-center">
+                  Let&apos;s talk
+                </button>
+              </a>
+            </p>
+          </section>
         </Container>
       </Section>
     </div>
