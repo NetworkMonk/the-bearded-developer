378fcabf4e13da03ccaf588fa5a238f62e541561
James Plant <jamesplant@gmail.com>
2025-09-10 13:56:40 +0100

    Add Sitemap component to generate dynamic site map from articles and routes

---
+++ src/app/sitemap.js
@@ -0,0 +1,26 @@
+import articleIndex from "@/data/article-index.json";
+
+export default async function Sitemap() {
+  const articles = articleIndex.articles.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
+  });
+
+  const baseUrl =
+    process.env.NEXT_PUBLIC_BASE_URL || "https://www.thebeardeddeveloper.co.uk";
+  const routes = [
+    "",
+    "/projects",
+    "/contact",
+    "/privacy",
+    "/cookies",
+    "/resources",
+    "/resources/components",
+    "/resources/articles",
+  ];
+
+  const posts = articles.map((article) => article.path.replace(/\\/g, "/"));
+
+  return [...routes, ...posts].map((url) => {
+    return { url: `${baseUrl}${url}` };
+  });
+}
